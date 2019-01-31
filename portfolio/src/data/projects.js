f551b60e0c6c41bec7d53360fe00fa351f0ebcff
Sarkis Kovlekjian <sako.orange@gmail.com>
2019-01-31 12:54:19 +0200

    Before sccrewing around

---
+++ portfolio/src/data/projects.js
@@ -0,0 +1,29 @@
+import project1 from '../assets/project1.png';
+import project2 from '../assets/project2.png';
+import project3 from '../assets/project3.png';
+
+const PROJECTS = [
+    {
+        id:1,
+        title: 'example React Applictaion',
+        description: 'A React App that i built, involving JS and core web concepts',
+        link: 'https://gitlab.com/Codfish-Group/jewelryhandshop-beirut',
+        image: project1
+    },
+    {
+        id:2,
+        title: 'Same Example React Applictaion',
+        description: 'A React App that i built, involving JS and core web concepts',
+        link: 'https://gitlab.com/Codfish-Group/jewelryhandshop-beirut',
+        image: project2
+    },
+    {
+        id:3,
+        title: 'Yet Another Example React Applictaion',
+        description: 'A React App that i built, involving JS and core web concepts',
+        link: 'https://gitlab.com/Codfish-Group/jewelryhandshop-beirut',
+        image: project3
+    }
+];
+
+export default PROJECTS;
---
+++ portfolio/src/data/socialProfiles.js
@@ -0,0 +1,32 @@
+import emailIcon from "../assets/email_icon.png";
+import githubIcon from "../assets/github_icon.png";
+import linkedinIcon from "../assets/linkedin_icon.png";
+import twitterIcon from "../assets/twitter_icon.png";
+
+const SOCIAL_PROFILES = [
+    {
+        id:1,
+        link: 'kovlekjian@gmail.com',
+        image: emailIcon
+    },
+
+    {
+        id:2,
+        link: 'https://github.com/kippo1234',
+        image: githubIcon
+    },
+
+    {
+        id:3,
+        link: 'https://www.linkedin.com/in/sarkis-kovlekjian/',
+        image: linkedinIcon
+    },
+
+    {
+        id:4,
+        link: 'https://twitter.com/',
+        image: twitterIcon
+    }
+
+];
+export default SOCIAL_PROFILES; 
